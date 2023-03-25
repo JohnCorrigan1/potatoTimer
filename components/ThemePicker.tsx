d6215866247b82b05cbc28f99151123dcd13e90d
John Corrigan <jcorrigan322@gmail.com>
2023-03-24 20:24:23 -0700

    theme generator

--- components/ThemePicker.tsx
+++ components/ThemePicker.tsx
@@ -12,12 +12,39 @@ useEffect(() => {
 
 <div className="dropdown dropdown-end">
   <label tabIndex={0} className="btn m-1 btn-ghost normal-case text-xl">Themes</label>
-  <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+  <ul tabIndex={0} className="dropdown-content menu p-2 w-[350px] shadow bg-base-100 rounded-box  max-h-[500px]  overflow-y-scroll">
+    <li data-set-theme="potato"><a>Potato</a></li>
     <li data-set-theme="light"><a>Light</a></li>
     <li data-set-theme="dark"><a>Dark</a></li>
     <li data-set-theme="cupcake"><a>Cupcake</a></li>
+    <li data-set-theme="bumblebee"><a>Bumblebee</a></li>
+    <li data-set-theme="emerald"><a>Emerald</a></li>
+    <li data-set-theme="corporate"><a>Corporate</a></li>
+    <li data-set-theme="synthwave"><a>Synthwave</a></li>
+    <li data-set-theme="retro"><a>Retro</a></li>
+    <li data-set-theme="cyberpunk"><a>Cyberpunk</a></li>
+    <li data-set-theme="valentine"><a>Valentine</a></li>
+    <li data-set-theme="halloween"><a>Halloween</a></li>
+    <li data-set-theme="garden"><a>Garden</a></li>
+    <li data-set-theme="forest"><a>Forest</a></li>
+    <li data-set-theme="aqua"><a>Aqua</a></li>
+    <li data-set-theme="lofi"><a>Lofi</a></li>
+    <li data-set-theme="pastel"><a>Pastel</a></li>
+    <li data-set-theme="fantasy"><a>Fantasy</a></li>
+    <li data-set-theme="wireframe"><a>Wireframe</a></li>
+    <li data-set-theme="black"><a>Black</a></li>
+    <li data-set-theme="luxury"><a>Luxury</a></li>
+    <li data-set-theme="dracula"><a>Dracula</a></li>
     <li data-set-theme="cmyk"><a>cmyk</a></li>
-  </ul>
+    <li data-set-theme="autumn"><a>Autumn</a></li>
+    <li data-set-theme="business"><a>Business</a></li>
+    <li data-set-theme="acid"><a>Acid</a></li>
+    <li data-set-theme="lemonade"><a>Lemonade</a></li>
+    <li data-set-theme="night"><a>Night</a></li>
+    <li data-set-theme="coffee"><a>Coffee</a></li>
+    <li data-set-theme="winter"><a>Winter</a></li>
+
+      </ul>
 </div>
    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,29 @@ module.exports = {
   },
   plugins: [require("daisyui")],
  daisyui: {
-    themes: ["cupcake", "dark", "cmyk", "light"],
+        themes: ["dark", "light", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter",
+                  {
+          potato: {
+
+ "primary": "#b45309",
+
+ "secondary": "#facc15",
+
+ "accent": "#4d7c0f",
+
+ "neutral": "#221D26",
+
+ "base-100": "#713f12",
+
+ "info": "#292524",
+
+ "success": "#158A47",
+
+ "warning": "#fde047",
+
+ "error": "#be123c",
+          },
+        },],
   },
 
 }
